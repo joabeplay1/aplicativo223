575db858013b7a40a1e2b590fad8983794c0beca
joabeplay1 <joabejonatas186@gmail.com>
2026-06-01 14:12:11 -0300

    Delete Projects.jsx

--- Projects.jsx
+++
@@ -1,4 +0,0 @@
-// Gerenciamento de projetos
-// Lista todos os projetos com filtro por categoria e pesquisa
-// Ações: abrir editor, duplicar, excluir
-// Usa: base44.entities.Project.list / delete / create
